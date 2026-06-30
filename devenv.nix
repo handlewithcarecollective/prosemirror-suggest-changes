ea93e29f4a0347475644e873af8bb8f55aa3fb09
Shane Friedman <smoores-gpg@friedmans.us>
2026-06-30 11:03:04 -0400

    Migrate to devenv from flake (#36)

---
+++ devenv.nix
@@ -0,0 +1,10 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  # https://devenv.sh/languages/
+  languages.javascript = {
+    enable = true;
+    corepack.enable = true;
+    package = pkgs.nodejs_24;
+  };
+}
